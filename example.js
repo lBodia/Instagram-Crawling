951f9f1577cea85fbda882e0509ca7bfa5393bff
lBodia <me@bodia.us>
2018-04-12 13:22:18 +0300

    Example

--- example.js
+++ example.js
@@ -5,13 +5,13 @@ let instagram = new InstagramCrawler;
 instagram.auth('veryactiveuser', '2Recognizer')
     .then(() => instagram.getTagMedia('kiev', 3))
     .then(media => {
-        console.log(media.length);
+        console.log(media);
 
-        // return instagram.getProfileMedia('instagram', 3)
+        return instagram.getProfileMedia('instagram', 3)
     })
-    // .then(media => {
-    //     console.log(media);
-    // })
+    .then(media => {
+        console.log(media);
+    })
     .catch(error => console.error(error));
 
 
